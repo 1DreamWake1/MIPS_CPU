e2f3cc862dad06d3cfcd3769d768d5ef167eaad8
1DreamWake1 <1127128563@qq.com>
2021-05-24 11:21:25 +0800

    将读改为组合逻辑，写改为时序逻辑

--- version1/source/RegFile.v
+++ version1/source/RegFile.v
@@ -2,12 +2,13 @@
  * @file	RegFile.v
  * @author	LiuChuanXi
  * @date	2021.04.28
- * @version	V1.0
+ * @version	V1.1
  * @brief	MIPS_CPU寄存器堆(RegFile)模块
  * @par	修改日志
  * <table>
  * <tr><th>Date			<th>Version		<th>Author		<th>Description
  * <tr><td>2021.04.28	<td>V1.0		<td>LiuChuanXi	<td>创建初始版本
+ * <tr><td>2021.05.23	<td>V1.1		<td>LiuChuanXi	<td>将读取改为组合逻辑，写入改为时序逻辑
  * </table>
  */
 
@@ -30,8 +31,13 @@
  * @param	regaData	output，寄存器A读出的数据
  * @param	regbData	output，寄存器B读出的数据
  * @warning	没有增加同时对同一个寄存器进行读写的隔离
+ * @warning	读取为组合逻辑，写入为时序逻辑
  */
-module RegFile(clk, rst, regaAddr, regaRd, regbAddr, regbRd, we, wAddr, wData, regaData, regbData);
+module RegFile(
+	clk, rst,
+	regaAddr, regaRd, regbAddr, regbRd, we, wAddr, wData,
+	regaData, regbData
+);
 
 	/* input */
 	input wire clk;									//时钟信号
@@ -61,29 +67,36 @@ module RegFile(clk, rst, regaAddr, regaRd, regbAddr, regbRd, we, wAddr, wData, r
 	end
 
 	/* 功能 */
-	always@(posedge clk, rst) begin
 
+	/* 复位信号rst */
+	always@(rst) begin
 		/* 复位信号rst有效 */
 		if(rst == `ENABLE) begin
 			/* 输出零，等价于输出零号寄存器(register[0])的值 */
-			regaData <= {`REG_LENGTH{1'b0}};
-			regbData <= {`REG_LENGTH{1'b0}};
+			regaData = {`REG_LENGTH{1'b0}};
+			regbData = {`REG_LENGTH{1'b0}};
 			/* 读取RegFile.txt初始化所有寄存器的值 */
 			$readmemh("MemFile/RegFile.txt", register);
 		end
-		/* 正常工作 */
-		else begin
-			/* 如果读使能则输出对应寄存器的值，否则输出零号寄存器(register[0])的值 */
-			regaData <= regaRd == `ENABLE ? register[regaAddr] : register[0];
-			regbData <= regbRd == `ENABLE ? register[regbAddr] : register[0];
-			/* 如果写信号we有效，则向目标寄存器写入 */
-			if(we == `ENABLE) begin
-				/* 警告：如果同时读写同一个寄存器会出现不可预料的错误，后续需要增加隔离操作 */
-				register[wAddr] <= wData;
-			end
-		end
-
 	end
 
+	/* 读取，组合逻辑 */
+	always@(*) begin
+		/* 复位信号rst无效 */
+		if(rst == `DISABLE) begin
+			/* 如果读使能则输出对应寄存器的值，否则输出零号寄存器(register[0])的值 */
+			regaData = (regaRd == `ENABLE) ? register[regaAddr] : register[0];
+			regbData = (regbRd == `ENABLE) ? register[regbAddr] : register[0];
+		end
+	end
+
+	/* 写入，时序逻辑 */
+	always@(posedge clk) begin
+		/* 复位信号rst无效，且写信号we有效 */
+		if((rst == `DISABLE) && (we == `ENABLE)) begin
+			/* 警告：如果同时读写同一个寄存器会出现不可预料的错误，后续需要增加隔离操作 */
+			register[wAddr] <= wData;
+		end
+	end
 
 endmodule
