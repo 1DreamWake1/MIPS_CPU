fd7750e05165db4d2e19d0efd8482d1b6178660b
1DreamWake1 <1127128563@qq.com>
2021-05-25 10:54:59 +0800

    rst和pc分成两个always块，修改PC初值为FC

--- version1/source/IF.v
+++ version1/source/IF.v
@@ -2,13 +2,14 @@
  * @file	IF.v
  * @author	LiuChuanXi
  * @date	2021.05.25
- * @version	V1.1
+ * @version	V1.2
  * @brief	MIPS CPU的取指令模块
  * @par	修改日志
  * <table>
  * <tr><th>Date			<th>Version		<th>Author		<th>Description
  * <tr><td>2021.04.28	<td>V1.0		<td>LiuChuanXi	<td>创建初始版本
  * <tr><td>2021.05.25	<td>V1.1		<td>LiuChuanXi	<td>整理包含头文件
+ * <tr><td>2021.05.25	<td>V1.2		<td>LiuChuanXi	<td>将rst和pc功能分成两个always块，pc初始值改为FC
  * </table>
  */
 
@@ -35,29 +36,34 @@ module IF(clk, rst, pc, romCe);
 
 	/* 模块初始化 */
 	initial begin
-
 		/* PC归零 */
-		pc = {`PC_LENGTH{1'b0}};
+		pc <= `PC_LENGTH'hFC;
 		/* 指令存储器InstMem模块使能 */
-		romCe = `ENABLE;
-
+		romCe <= `DISABLE;
 	end
 
-	/* 功能 */
-	always@(posedge clk, rst) begin
-
+	/* 复位信号rst */
+	always@(rst) begin
 		/* 复位信号rst有效 */
 		if(rst == `ENABLE) begin
 			/* PC归零 */
-			pc = {`PC_LENGTH{1'b0}};
+			pc <= `PC_LENGTH'hFC;
 			/* 指令存储器InstMem模块使能 */
-			romCe = `ENABLE;
+			romCe <= `DISABLE;
 		end
-		/* 正常工作，PC自加(`PC_STEP) */
-		else begin
+	end
+
+	/* PC正常工作 */
+	always@(posedge clk) begin
+		/**
+		 * 复位信号rst无效
+		 * 正常工作，PC自加(`PC_STEP)
+		 * InstMem指令存储器使能信号romCe有效
+		 */
+		if(rst == `DISABLE) begin
+			romCe <= `ENABLE;
 			pc <= pc + `PC_STEP;
 		end
-
 	end
 
 
