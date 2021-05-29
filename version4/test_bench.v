760fe25c33982f577a6c60e7a44b2aa3cc3a45cc
1DreamWake1 <1127128563@qq.com>
2021-05-29 15:01:03 +0800

    修改仿真时间为2000个时间单位

--- version4/test_bench.v
+++ version4/test_bench.v
@@ -1,18 +1,21 @@
 /**
  * @file	test_bench.v
  * @author	LiuChuanXi
- * @date	2021.05.25
- * @version	V1.1
+ * @date	2021.05.29
+ * @version	V4.0
  * @brief	MIPS_CPU顶层测试模块
  * @par	修改日志
  * <table>
  * <tr><th>Date			<th>Version		<th>Author		<th>Description
  * <tr><td>2021.04.28	<td>V1.0		<td>LiuChuanXi	<td>创建初始版本
-  * <tr><td>2021.05.25	<td>V1.1		<td>LiuChuanXi	<td>整理包含头文件
+ * <tr><td>2021.05.25	<td>V1.1		<td>LiuChuanXi	<td>整理包含头文件
+ * <tr><td>2021.05.29	<td>V4.0		<td>LiuChuanXi	<td>修改仿真时间为2000个单位
  * </table>
  */
 
+
 `timescale 1ns/1ns
+
 
 `define LINUX_VERILOG //Verilog in Linux
 
@@ -24,9 +27,8 @@
 module test_bench();
 
 	/* private */
-	reg clk;
-	reg rst;
-
+	reg clk;			//始终信号
+	reg rst;			//复位信号
 
 	/* module */
 	SOC soc_m(
@@ -37,9 +39,8 @@ module test_bench();
 	/* 测试模块 */
 	initial begin
 		#0	clk = 1'b0;
-			rst = `ENABLE;
+		#0	rst = `ENABLE;
 		#30	rst = `DISABLE;
-
 	end
 
 
@@ -52,13 +53,11 @@ module test_bench();
 	/* Linux下测试模块 */
 `ifdef LINUX_VERILOG
 	initial begin
-
 		$dumpfile("./build/wave.vcd");
 		$dumpvars(0, test_bench);
-
-		#1000
+		/* 仿真时间 */
+		#2000
 			$stop(2);
-
 	end
 `endif //LINUX_VERIOLG
 
